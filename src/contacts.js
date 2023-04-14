c478317622e708e05cc4db8c21a22a280152b68f
d ravi <108458982+ravi944@users.noreply.github.com>
2023-04-14 11:31:27 +0530

    Add files via upload

---
+++ src/contacts.js
@@ -0,0 +1,42 @@
+const contacts = [
+  {
+    id: "1",
+    name: "Dhanush Gowda",
+    imgURL:
+      "https://www.pinkvilla.com/imageresize/celebs_tribute_to_puneeth_rajkumar.jpg?width=752&t=pvorg",
+    phone: "+91 6362566362",
+    email: "sanjay.143@gmail.com"
+  },
+  {
+    id: "2",
+    name: "Nitheesh",
+    imgURL:
+      "https://www.winnerdesignsolutions.in/assets/images/nitheesh%20sir.jpg",
+    phone: "+91 9876543210",
+    email: "wds@gmail.com"
+  },
+  {
+    id: "3",
+    name:"Prabhas Raju",
+    imgURL:"https://i.pinimg.com/736x/55/dc/13/55dc134e11c2386f6fd1a40ae69626cf.jpg",
+    phone:"+92 9493752089",
+    email:"gmail@prabhas.com"
+  },
+  {
+    id: "4",
+    name:"Virat Kohli",
+    image:"https://m.cricbuzz.com/a/img/v1/192x192/i1/c244980/virat-kohli.jpg",
+    phone:"+91 6362560000",
+    email:"virat.rcb@gmail.com"
+  },
+  {
+    id: "5",
+    name:"Kalpana Chawla",
+    image:"https://static.javatpoint.com/biography/images/kalpana-chawla.png",
+    phone:"+1 8374656322",
+    email:"kcdhanush@yahoo.com"
+  }
+    
+];
+
+export default contacts;
